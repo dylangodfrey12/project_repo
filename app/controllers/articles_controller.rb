8cda2823c7963a321c145133874302de7e92a3b7
Rails Udemy Course <dylangodfrey12@gmail.com>
2017-11-26 15:36:14 +0000

    remove reduncy prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   
   
   def index
@@ -8,7 +9,7 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   def edit
-@article = Article.find(params[:id])
+
   end
   def create
     @article = Article.new(article_params)
@@ -19,9 +20,9 @@ class ArticlesController < ApplicationController
    else
      render 'new'
    end
- end
+  end
       def update
-        @article = Article.find(params[:id])
+        
         if @article.update(article_params)
           flash[:notice] = "article successfully updated"
           redirect_to article_path(@article)
@@ -29,17 +30,21 @@ class ArticlesController < ApplicationController
           render 'edit'
         end
     
-    end
+       end
         def show
-          @article = Article.find(params[:id])
+          
   end
+  
   def destroy
-    @article = Article.find(params[:id])
+   
     @article.destroy
     flash[:notice] = "Article was succesfully deleted"
     redirect_to articles_path
   end
    private 
+    def set_article
+      @article = Article.find(params[:id])
+    end
    def article_params
      params.require(:article).permit(:title, :description)
    end
