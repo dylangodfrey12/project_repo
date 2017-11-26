9a060eaf9b55f05e3e950aa1f7bdda0cd298a385
Rails Udemy Course <dylangodfrey12@gmail.com>
2017-11-26 21:54:02 +0000

    styled errors and messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
    if  @article.save
-      flash[:notice] = "Article was created"
+      flash[:success] = "Article was created"
       redirect_to article_path(@article)
 
    else
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
       def update
         
         if @article.update(article_params)
-          flash[:notice] = "article successfully updated"
+          flash[:success] = "article successfully updated"
           redirect_to article_path(@article)
         else
           render 'edit'
@@ -38,7 +38,7 @@ class ArticlesController < ApplicationController
   def destroy
    
     @article.destroy
-    flash[:notice] = "Article was succesfully deleted"
+    flash[:danger] = "Article was succesfully deleted"
     redirect_to articles_path
   end
    private 
