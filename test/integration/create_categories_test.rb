d72035993bb633fd80bc8dec389afb44786a4176
Rails Udemy Course <dylangodfrey12@gmail.com>
2017-12-23 00:58:13 +0000

    completed blog

---
+++ test/integration/create_categories_test.rb
@@ -0,0 +1,65 @@
+require 'test_helper'
+
+class CreateCategoriesTest < ActionDispatch::IntegrationTest
+
+def setup
+
+@user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+
+end
+
+test "get new category form and create category" do
+
+sign_in_as(@user, "password")
+
+get new_category_path
+
+assert_template 'categories/new'
+
+assert_difference 'Category.count', 1 do
+
+post_via_redirect categories_path, category: {name: "sports"}
+
+# Note the line above was different for Rails 5
+
+end
+
+assert_template 'categories/index'
+
+assert_match "sports", response.body
+
+end
+
+test "invalid category submission results in failure" do
+
+sign_in_as(@user, "password")
+
+get new_category_path
+
+assert_template 'categories/new'
+
+assert_no_difference 'Category.count' do
+
+post categories_path, category: {name: " "}
+
+# Note the line above was different for Rails 5
+
+end
+
+assert_template 'categories/new'
+
+assert_select 'h2.panel-title'
+
+assert_select 'div.panel-body'
+
+end
+
+end
+
+Add sign_in_user method to test_helper.rb file under test folder:
+
+def sign_in_as(user, password)
+
+post login_path, session: {email: user.email, password: password}
+
+end
